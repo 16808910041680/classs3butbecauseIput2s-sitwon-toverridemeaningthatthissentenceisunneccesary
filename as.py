eea87c55ac4bea1f7df0c05ee8c77d9a4e8f0344
16808910041680 <forforfirefire@gmail.com>
2025-03-16 11:08:20 +1100

    hi

--- as.py
+++ as.py
@@ -3,4 +3,7 @@ b = int(a)
 print (b, "2")
 type (int)
 c = input ("Please give us a random name")
-print ("Hello", c, ", This is a random message." )
+print ("Hello", ", This is a random message." )
+d = "Absolutely Nothing"
+e = int (input ("random number please"))
+print (d, e + b, e*b, '/n', d, "Hello", c, "Here's Your Number (s):",e+b, e*e, b/e, b+e*b+b+e) 
